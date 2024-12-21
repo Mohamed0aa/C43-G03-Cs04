99d9e5cf436fa52298c3c5a1a689004140bb1e67
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-21 11:11:32 +0200

    q2

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -5,12 +5,16 @@
         static void Main(string[] args)
         {
             #region q1 :  Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
-            int n = int.Parse(Console.ReadLine());
-            for(int i = 1; i <= n; i++) 
-                Console.Write(i+" ");
+            //int n = int.Parse(Console.ReadLine());
+            //for(int i = 1; i <= n; i++) 
+            //    Console.Write(i+" ");
             #endregion
 
-            
+            #region q2 :  Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //    Console.Write(i*n+" ");
+            #endregion
         }
     }
 }
