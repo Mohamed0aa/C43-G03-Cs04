2300ceb7082377b5da621771bf1aebf6ce79316a
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-21 11:07:29 +0200

    q1

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region q1 :  Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++) 
+                Console.Write(i+" ");
+            #endregion
+
+            
         }
     }
 }
